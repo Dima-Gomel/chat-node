88921dc2750a42a173bc42c4dfdd3a98e52cb0b0
Dima <metalland2015@yandex.ru>
2019-10-25 19:18:41 +0300

    111

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const WebSocket = require('ws');
+
+const port = process.env.PORT || 3000; 
+
+const app = express();
+const server = app.listen(port, () => {
+    console.log('Server stated on port: ', port);
+});
+
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws) => {
+    ws.on('message', (message) => {
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });  
+    
+});
